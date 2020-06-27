d5e46f3edf445d28e55d791317f8f6b08f87d371
Dzianis_Slava1 <dzianis_slava1@epam.com>
2020-06-27 18:58:35 +0300

    done2

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -12,14 +12,20 @@ const plus = (a + b == 4)? "Wrong":"Too much";
 
 /** TODO
  * The system receives two params x and y.
- * Implement the logic when z equal to sum of x and y in case both of them less than 10 or multiplication in case of greater or equal than 10
+ * Implement the logic when z equals to sum of x and y in case both of them
+ * less than 10 or multiplication in case of greater or equal than 10
  * Task 1 - if else
  * Task 2 - ternary operator
  */
-{
+
     const x = 3;
     const y = 7;
-}
+    const z = x + y;
+
+    if (x + y < 10) {
+      console.log('That is right');
+    }
+
 
 /** TODO
  * The system receives 3 params - x, y, operator.
@@ -31,4 +37,18 @@ const plus = (a + b == 4)? "Wrong":"Too much";
     const x = 3;
     const y = 7;
     const operator = "add";
+}
+function operators(x, y, operator) {
+  switch(operator) {
+    case "add":
+      return x + y;
+  case "subtract":
+      return x - y;
+  case "multiply":
+      return x * y;
+  case "devide":
+      return x / y;
+  default:
+      return "Operator isn't recognized";
+}
 }
